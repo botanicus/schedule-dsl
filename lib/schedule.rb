dbbbbe431cedb83b908f9ae91d34688260dc05c6
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-12-03 10:36:00 +0000

    Add RuleList, so we can evaluate

--- lib/schedule.rb
+++ lib/schedule.rb
@@ -1,9 +1,28 @@
+require 'forwardable'
+
 module Schedule
   # Load schedule from definition file.
   def self.load(path)
     context = DSL.new
     context.instance_eval(File.read(path), path)
     context.rules
+  end
+
+  class RuleList
+    extend Forwardable
+
+    attr_reader :rules
+    def initialize
+      @rules = Array.new
+    end
+
+    def_delegators :rules, :length, :first, :empty?, :<<
+
+    def evaluate(date)
+      self.rules.select do |rule|
+        rule.call(date)
+      end
+    end
   end
 
   class Rule
@@ -16,7 +35,7 @@ module Schedule
   class DSL
     attr_reader :rules
     def initialize
-      @rules = Array.new
+      @rules = RuleList.new
     end
 
     def rule(condition, &block)
